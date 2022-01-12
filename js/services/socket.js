eb785fe06e899535f72e9d4da80adecffcf6cbd8
muertet <fp557s@gmail.com>
2022-01-12 22:28:53 +0100

    + Determine websocket host automatically

--- js/services/socket.js
+++ js/services/socket.js
@@ -8,7 +8,7 @@ class Sockets {
 
     connect () {
         return new Promise((resolve, reject) => {
-            this.client = new W3CWebSocket('ws://localhost:8081/', localStorage.getItem("token"));
+            this.client = new W3CWebSocket('ws://' + location.host + '/', localStorage.getItem("token"));
 
             this.client.onopen = () => {
                 console.log('WebSocket Client Connected');
